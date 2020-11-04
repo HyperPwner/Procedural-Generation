ca0fe4bf8df0d26b7b468ab43def085685889342
Cameron Lillie <CammieLil@hotmail.com>
2020-11-04 11:37:11 +0000

    uploading work

---
+++ Procedural Animations/Assets/Scripts/Editor/AnimationGeneratorEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(AnimationGenerator))]
+public class AnimationGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AnimationGenerator AnimGen = (AnimationGenerator)target;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate!"))
+        {
+            AnimGen.Generate();
+        }
+    }
+}
